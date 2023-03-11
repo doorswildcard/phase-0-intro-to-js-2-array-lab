bedbca93d98db07b5e9654d95e41fe572e6eca13
Ilan Rojas <bobatthevelvetroom@gmail.com>
2023-03-11 07:08:32 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function removeFirstCat(){
+
+    return cats.slice(1);
+
+}
+
+function removeLastCat(){
+
+   return cats.slice(0, cats.length - 1);
+
+}
+
+function appendCat(name){
+ const newCats = [...cats, name]
+ return newCats;
+}
+
+function prependCat(name){
+    const newCats = [name,...cats]
+    return newCats;
+}
